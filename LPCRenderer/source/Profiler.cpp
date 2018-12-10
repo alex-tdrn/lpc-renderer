f4920935191da0c910dd888724f128b9a9fc51a8
Alex Todoran <todoranalex97@gmail.com>
2018-12-10 13:04:29 +0100

    context info improvements

--- LPCRenderer/source/Profiler.cpp
+++ LPCRenderer/source/Profiler.cpp
@@ -124,14 +124,40 @@ void Profiler::drawUI()
 	}
 
 	ImGui::NewLine();
-	if(ImGui::CollapsingHeader("GL Context"))
+	static bool showContextInfo = false;
+	if(ImGui::Button("GL Context"))
+		showContextInfo = true;
+	if(showContextInfo)
 	{
+		ImGui::Begin("GL Context", &showContextInfo);
 		struct GLContext
 		{
 			int majorVersion;
 			int minorVersion;
 			std::string vendor;
 			std::string device;
+			int maxShaderStorageBufferBytes;
+			int maxShaderStorageBufferKiloBytes;
+			int maxShaderStorageBufferMegaBytes;
+			int maxShaderStorageBufferGigaBytes;
+			int maxUniformBufferBytes;
+			int maxUniformBufferKiloBytes;
+			int maxUniformBufferMegaBytes;
+			int maxUniformBufferGigaBytes;
+			int maxVertexUniforms;
+			int maxGeometryUniforms;
+			int maxFragmentUniforms;
+			int maxVertexUniformComponents;
+			int maxFragmentUniformComponents;
+			int maxGeometryUniformComponents;
+			int maxGeometryInputComponents;
+			int maxGeometryOutputComponents;
+			int maxGeometryTotalOutputComponents;
+			int maxGeometryOutputVertices;
+			int maxGeometryShaderInvocations;
+			int maxVertices;
+			int maxVertexAttributes;
+			int maxVertexTextureUnits;
 			std::vector<std::string> supportedExtensions;
 		};
 		static GLContext const context = []() -> GLContext{
@@ -140,6 +166,35 @@ void Profiler::drawUI()
 			glGetIntegerv(GL_MINOR_VERSION, &ret.minorVersion);
 			ret.vendor = reinterpret_cast<char const*>(glGetString(GL_VENDOR));
 			ret.device = reinterpret_cast<char const*>(glGetString(GL_RENDERER));
+
+			glGetIntegerv(GL_MAX_SHADER_STORAGE_BLOCK_SIZE, &ret.maxShaderStorageBufferBytes);
+			ret.maxShaderStorageBufferKiloBytes = ret.maxShaderStorageBufferBytes >> 10;
+			ret.maxShaderStorageBufferMegaBytes = ret.maxShaderStorageBufferKiloBytes >> 10;
+			ret.maxShaderStorageBufferGigaBytes = ret.maxShaderStorageBufferMegaBytes >> 10;
+
+			glGetIntegerv(GL_MAX_UNIFORM_BLOCK_SIZE, &ret.maxUniformBufferBytes);
+			ret.maxUniformBufferKiloBytes = ret.maxUniformBufferBytes >> 10;
+			ret.maxUniformBufferMegaBytes = ret.maxUniformBufferKiloBytes >> 10;
+			ret.maxUniformBufferGigaBytes = ret.maxUniformBufferMegaBytes >> 10;
+
+			glGetIntegerv(GL_MAX_GEOMETRY_INPUT_COMPONENTS, &ret.maxGeometryInputComponents);
+			glGetIntegerv(GL_MAX_GEOMETRY_OUTPUT_COMPONENTS, &ret.maxGeometryOutputComponents);
+			glGetIntegerv(GL_MAX_GEOMETRY_TOTAL_OUTPUT_COMPONENTS, &ret.maxGeometryTotalOutputComponents);
+			glGetIntegerv(GL_MAX_GEOMETRY_OUTPUT_VERTICES, &ret.maxGeometryOutputVertices);
+			glGetIntegerv(GL_MAX_GEOMETRY_SHADER_INVOCATIONS, &ret.maxGeometryShaderInvocations);
+
+			glGetIntegerv(GL_MAX_VERTEX_UNIFORM_BLOCKS, &ret.maxVertexUniforms);
+			glGetIntegerv(GL_MAX_GEOMETRY_UNIFORM_BLOCKS, &ret.maxGeometryUniforms);
+			glGetIntegerv(GL_MAX_FRAGMENT_UNIFORM_BLOCKS, &ret.maxFragmentUniforms);
+
+			glGetIntegerv(GL_MAX_VERTEX_UNIFORM_COMPONENTS, &ret.maxVertexUniformComponents);
+			glGetIntegerv(GL_MAX_GEOMETRY_UNIFORM_COMPONENTS, &ret.maxGeometryUniformComponents);
+			glGetIntegerv(GL_MAX_FRAGMENT_UNIFORM_COMPONENTS, &ret.maxFragmentUniformComponents);
+
+			glGetIntegerv(GL_MAX_ELEMENTS_VERTICES, &ret.maxVertices);
+			glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &ret.maxVertexAttributes);
+			glGetIntegerv(GL_MAX_VERTEX_TEXTURE_IMAGE_UNITS, &ret.maxVertexTextureUnits);
+
 			int nSupportedExtensions;
 			glGetIntegerv(GL_NUM_EXTENSIONS, &nSupportedExtensions);
 			for(int i = 0; i < nSupportedExtensions; i++)
@@ -149,15 +204,74 @@ void Profiler::drawUI()
 		ImGui::Text("API: OpenGL %i.%i", context.majorVersion, context.minorVersion);
 		ImGui::Text("Vendor: %s", context.vendor.data());
 		ImGui::Text("Device: %s", context.device.data());
-		if(ImGui::TreeNode("Supported Extensions:"))
+		ImGui::Separator();
+		ImGui::Text("Max Shader Storage Buffer Size: ");
+		ImGui::SameLine();
+		if(context.maxShaderStorageBufferGigaBytes)
 		{
-			ImGui::Indent();
-			ImGui::BeginChild("Supported Extensions", {0, 100});
-			for(auto extension : context.supportedExtensions)
-				ImGui::Text(extension.data());
-			ImGui::EndChild();
-			ImGui::Unindent();
-			ImGui::TreePop();
+			ImGui::Text("%lu GB ", context.maxShaderStorageBufferGigaBytes);
+			ImGui::SameLine();
+			ImGui::Text("%lu MB ", context.maxShaderStorageBufferMegaBytes - (context.maxShaderStorageBufferGigaBytes << 10));
 		}
+		else if(context.maxShaderStorageBufferMegaBytes)
+		{
+			ImGui::Text("%lu MB ", context.maxShaderStorageBufferMegaBytes);
+			ImGui::SameLine();
+			ImGui::Text("%lu KB ", context.maxShaderStorageBufferKiloBytes - (context.maxShaderStorageBufferMegaBytes << 10));
+		}
+		else if(context.maxShaderStorageBufferKiloBytes)
+		{
+			ImGui::Text("%lu KB ", context.maxShaderStorageBufferKiloBytes);
+			ImGui::SameLine();
+			ImGui::Text("%lu B ", context.maxShaderStorageBufferBytes - (context.maxShaderStorageBufferKiloBytes << 10));
+		}
+		else
+		{
+			ImGui::Text("%lu B ", context.maxShaderStorageBufferBytes);
+		}
+
+		ImGui::Text("Max Uniform Buffer Size: ");
+		ImGui::SameLine();
+		if(context.maxUniformBufferGigaBytes)
+		{
+			ImGui::Text("%lu GB ", context.maxUniformBufferGigaBytes);
+			ImGui::SameLine();
+			ImGui::Text("%lu MB ", context.maxUniformBufferMegaBytes - (context.maxUniformBufferGigaBytes << 10));
+		}
+		else if(context.maxUniformBufferMegaBytes)
+		{
+			ImGui::Text("%lu MB ", context.maxUniformBufferMegaBytes);
+			ImGui::SameLine();
+			ImGui::Text("%lu KB ", context.maxUniformBufferKiloBytes - (context.maxUniformBufferMegaBytes << 10));
+		}
+		else if(context.maxUniformBufferKiloBytes)
+		{
+			ImGui::Text("%lu KB ", context.maxUniformBufferKiloBytes);
+			ImGui::SameLine();
+			ImGui::Text("%lu B ", context.maxUniformBufferBytes - (context.maxUniformBufferKiloBytes << 10));
+		}
+		else
+		{
+			ImGui::Text("%lu B ", context.maxUniformBufferBytes);
+		}
+		ImGui::Text("Max Uniforms: %i, %i, %i", context.maxVertexUniforms, context.maxGeometryUniforms, context.maxFragmentUniforms);
+		ImGui::Text("Max Uniform Components: %i, %i, %i", context.maxVertexUniformComponents, context.maxGeometryUniformComponents, context.maxFragmentUniformComponents);
+		ImGui::NewLine();
+		ImGui::Text("Max Geometry Input Components: %i", context.maxGeometryInputComponents);
+		ImGui::Text("Max Geometry Output Components: %i", context.maxGeometryOutputComponents);
+		ImGui::Text("Max Geometry Total Output Components: %i", context.maxGeometryTotalOutputComponents);
+		ImGui::Text("Max Geometry Output Vertices: %i", context.maxGeometryOutputVertices);
+		ImGui::Text("Max Geometry Shader Invocations: %i", context.maxGeometryShaderInvocations);
+		ImGui::NewLine();
+		ImGui::Text("Max VAO Vertices: %i", context.maxVertices);
+		ImGui::Text("Max Vertex Attributes: %i", context.maxVertexAttributes);
+		ImGui::Text("Max Vertex Texture Units: %i", context.maxVertexTextureUnits);
+		ImGui::Separator();
+		ImGui::Text("Supported Extensions");
+		ImGui::Indent();
+		for(auto extension : context.supportedExtensions)
+			ImGui::Text(extension.data());
+		ImGui::Unindent();
+		ImGui::End();
 	}
 }
